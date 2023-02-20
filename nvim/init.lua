6effbf416c010eac534fa2088de3dea1da1bc322
Ray Wong <raycnwong@gmail.com>
2023-02-20 13:57:15 +0900

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.cmd.colorscheme('everblush')
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+
+require 'remap'
+require 'plugins'
+
---
+++ nvim/lua/remap.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('v', '∆', ":m '>+1<CR>gv=gv") -- alt + k
+vim.keymap.set('v', '˚', ":m '<-2<CR>gv=gv") -- alt + j
