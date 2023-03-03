5254f9e59f1c28913817d045b46b1cce27bc8ec2
Ray Wong <ray.wong@hennge.com>
2023-03-03 10:23:50 +0900

    add keymap to run :noh

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,2 +1,3 @@
-vim.keymap.set('v', 'K', ":m '>+1<CR>gv=gv") -- shift + k
-vim.keymap.set('v', 'J', ":m '<-2<CR>gv=gv") -- shift + j
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- shift + j
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- shift + k
+vim.keymap.set('n', '<space>n', ":noh<CR>")
