c16b2428a5107c1a5e5161d77e200881bb77e62f
Ray Wong <ray.wong@hennge.com>
2023-05-12 23:14:37 +0900

    add pane navigation keymap

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -3,3 +3,7 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- shift + k
 vim.keymap.set('n', '<leader>n', ":noh<CR>")
 vim.keymap.set('x', '<leader>p', "\"_dP")
 vim.keymap.set('n', '<leader>g', ":Git<CR>")
+vim.keymap.set('n', '<C-h>', "<C-w>h")
+vim.keymap.set('n', '<C-j>', "<C-w>j")
+vim.keymap.set('n', '<C-k>', "<C-w>k")
+vim.keymap.set('n', '<C-l>', "<C-w>l")
