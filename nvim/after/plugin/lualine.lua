d995b17c93f64f557560b18b80c9434bc0685d6c
Ray Wong <ray.wong@hennge.com>
2023-07-28 11:58:18 +0900

    update colorscheme and lualine styles

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -5,8 +5,8 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'everblush',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = '|',
+    section_separators = '',
     disabled_filetypes = {
       statusline = {},
       winbar = {},
