30983ee7150ed9a61acf2e5c9dd2c614e3791447
Ray Wong <ray.wong@hennge.com>
2023-10-02 14:49:56 +0900

    add telescope file browser

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,4 +40,9 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { { 'nvim-lua/plenary.nvim' } },
   }
+
+  use {
+    'nvim-telescope/telescope-file-browser.nvim',
+    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  }
 end)
